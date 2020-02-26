02f16282b780d09ff627ce5055e31fc4a9d04be7
Jhon.beltran <jhon.beltran@payulatam.com>
2020-02-26 18:54:56 -0500

    add example crud using lists

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 # Using __name__ we give a unique name to our Flask app
 app = Flask(__name__)
 
+payers = []
 
-@app.route('/')
-def home():
-    return 'Hola'
+
+@app.route('/payer', methods=['GET'])
+def get_payers():
+    return jsonify(payers)
+
+
+@app.route('/payer/<int:payer_id>', methods=['GET'])
+def validate_payer(payer_id):
+    for payer in payers:
+        if payer['payer_id'] == payer_id:
+            return jsonify(payer)
+        else:
+            return f'Payer {payer_id} not found'
+
+
+@app.route('/payer', methods=['POST'])
+def add_payer():
+    payers.append(request.get_json())
+    return f'Payer added {request.get_json()}'
+
+
+@app.route('/payer', methods=['DELETE'])
+def remove_payer():
+    payer_id = request.get_json()['payer_id']
+    for payer in payers:
+        if payer['payer_id'] == payer_id:
+            payers.remove(payer)
+            return f'Payer {payer_id} removed'
+        else:
+            return f'Payer {payer_id} not found'
 
 
 app.run(port=5000)
