d51e0c9728d7b2bb6fe447848ebc5dab5f759f53
Jhon Beltran <jbeltranleon@gmail.com>
2020-02-29 12:59:51 -0500

    add basic structure to authenticate users

---
+++ security.py
@@ -0,0 +1,20 @@
+from .user import User
+from werkzeug.security import safe_str_cmp
+
+users = [
+    User(1, 'jhon', '123')
+]
+
+username_mapping = {u.username: u for u in users}
+user_id_mapping = {u.id: u for u in users}
+
+
+def authenticate(username, password):
+    user = user_id_mapping.get(username, None)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return user_id_mapping.get(user_id, None)
